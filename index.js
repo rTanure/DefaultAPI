84a5482c145a0d74c1ff6cce522249763c540dad
Richardy Tanure <richardy.tanure@hotmail.com>
2022-07-05 11:02:10 -0300

    CORS fixed

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
 
 const PORT = process.env.PORT || 8080
 
